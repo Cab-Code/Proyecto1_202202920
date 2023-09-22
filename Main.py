28d3ffd2b8dd341fad0fd17570c9080794f6f867
Cab-Code <estuardocabrera10c@gmail.com>
2023-09-21 22:08:11 -0600

    commit 2 202202920 Estuardo Cabrera

--- Main.py
+++ Main.py
@@ -1,33 +1,78 @@
 import tkinter as tk
 from tkinter import ttk 
 
-#PAleta de Colores
+#Paleta de Colores
 BGclr = '#fef6cd'
 almClr = '#20c67a'
 prfClr = '#3f8880'
 admClr = '#9dc09d'
 extClr = '#a0fb0e'
 
-def getUserData (paramUno, paramDos, paramTres):
+#Inicio de ventana 
+iniSecion = tk.Tk()
+contenedor = tk.Frame(iniSecion,padx = 50, pady = 60, width = 600, height = 400)
+instruccion = tk.Label(master = contenedor, text = 'Iniciar secion como...')
+btnAlumno = tk.Button(master = contenedor, text = 'Alumno', width = 100, relief = 'flat', bg = almClr, command = lambda:SecionAlumno(contenedor, btnAdmin, btnAlumno, btnProfesor, instruccion, iniSecion))
+btnProfesor= tk.Button(master = contenedor, text = 'Profesor', width = 100, relief = 'flat', bg = prfClr)
+btnAdmin = tk.Button(master = contenedor, text = 'Administrador', width = 100, relief = 'flat', bg = admClr)
+
+def Inicio (iniSecion):
+    
+    alto = iniSecion.winfo_reqheight()
+    largo = iniSecion.winfo_reqwidth()
+
+    
+    contenedor['bg'] = BGclr
+    instruccion.pack(pady = 20)
+    instruccion['bg'] = BGclr
+
+
+   
+    contenedor.pack(pady = alto/2 - 20, padx = 100) 
+    btnAdmin.pack(pady = 10)
+    btnProfesor.pack(pady = 10)
+    btnAlumno.pack(pady = 10)
+    iniSecion.update()
+
+
+def getUserData (tipo, paramUno, paramDos, paramTres):
+    if tipo == 'alumno':
         carnet = paramUno.get()
         userName = paramDos.get()
         password = paramTres.get()
-
         print(carnet, userName, password)
+    elif tipo == 'profesor':
+        print('Profesor')
+    elif tipo == 'Admin':
+        print('Administrador')
+
+
+        
 
 def borrarWidgets(widgets):
     for widget in widgets:
         widget.pack_forget()
 
+def restaurar(widgets, w):
+    borrarWidgets(widgets)
 
-def SecionAlumno (contenedor, admin, alumno, profesor, instruc):
+    contenedor['bg'] = BGclr
+    instruccion['bg'] = BGclr
+    instruccion['text'] = 'Iniciar secion como...'
+    btnAdmin.pack(pady = 10)
+    btnProfesor.pack(pady = 10)
+    btnAlumno.pack(pady = 10)
+
+
+
+def SecionAlumno (contenedor, admin, alumno, profesor, instruc, w):
        
-
-    contenedor['bg'] = almClr
     borrarWidgets([admin, alumno, profesor])
+    contenedor['bg'] = almClr
     instruc['text'] = 'Ingrese sus datos'
     instruc['bg'] = almClr
 
+    
 
     LabelA = tk.Label(master = contenedor, text = 'Carnet', bg = almClr)
     getCarnet = tk.Entry(master = contenedor, width = 100, bg = BGclr, relief = 'flat')
@@ -37,7 +82,7 @@ def SecionAlumno (contenedor, admin, alumno, profesor, instruc):
 
     LabelC = tk.Label(master = contenedor, text = 'Contraseña', bg = almClr,)
     getPassword = tk.Entry(master = contenedor, width = 100, bg = BGclr, relief = 'flat')
-    getPassword.config(show="*")
+    getPassword['show'] = '*'
 
     LabelA.pack(pady = 5)
     getCarnet.pack(pady = 10)
@@ -48,38 +93,18 @@ def SecionAlumno (contenedor, admin, alumno, profesor, instruc):
 
     botones = tk.Frame(contenedor ,padx = 50, pady = 20, bg = almClr)
     botones.pack()
-    ingreso = tk.Button(master = botones, text = 'Ingresar', bg = extClr, relief = 'flat', command = lambda: getUserData(getCarnet, getUserName, getPassword))
+    ingreso = tk.Button(master = botones, text = 'Ingresar', bg = extClr, relief = 'flat', command = lambda: getUserData('alumno',getCarnet, getUserName, getPassword))
     ingreso.grid(column = 0, row = 0, padx = 40)
-    registro = tk.Button(master = botones, text = 'registrarse', bg = extClr, relief = 'flat')
+    registro = tk.Button(master = botones, text = 'Nuevo Usuario', bg = extClr, relief = 'flat')
     registro.grid(column = 2, row = 0, padx = 40)
+    regresar = tk.Button(master = botones, text = 'regresar', bg = extClr, relief = 'flat', command = lambda: restaurar([LabelA, LabelB, LabelC, getCarnet, getPassword, getUserName, botones, ingreso, registro, regresar],w))
+    regresar.grid(column = 4, row = 0, padx = 40)
 
 
 
-def Inicio (iniSecion):
     
-    alto = iniSecion.winfo_reqheight()
-    largo = iniSecion.winfo_reqwidth()
 
-    contenedor = tk.Frame(iniSecion,padx = 50, pady = 60, width = 600, height = 400)
-    contenedor['bg'] = BGclr
-    instruccion = tk.Label(master = contenedor, text = 'Iniciar secion como...')
-    instruccion.pack(pady = 20)
-    instruccion['bg'] = BGclr
-
-
-    btnAlumno = tk.Button(master = contenedor, text = 'Alumno', width = 100, relief = 'flat', bg = almClr, command = lambda:SecionAlumno(contenedor, btnAdmin, btnAlumno, btnProfesor, instruccion))
-    btnProfesor= tk.Button(master = contenedor, text = 'Profesor', width = 100, relief = 'flat', bg = prfClr)
-    btnAdmin = tk.Button(master = contenedor, text = 'Administrador', width = 100, relief = 'flat', bg = admClr)
-
-    contenedor.pack(pady = alto/2 - 20, padx = 100) 
-    btnAdmin.pack(pady = 10)
-    btnProfesor.pack(pady = 10)
-    btnAlumno.pack(pady = 10)
-    iniSecion.update()
-    
-#Inicio de ventana 
 #Flujo de la aplicacion
-iniSecion = tk.Tk()
 Inicio(iniSecion)
 iniSecion.title('Academia USAC  iniciar secion')
 iniSecion['bg'] = BGclr
--- pruebas.py
+++ pruebas.py
@@ -1,11 +1,22 @@
+alumnos = []
+
 with open('data.txt') as file:
     leer = file.read()
 
     data = leer.split('\n')
-    alumnos = []
     for elemento in data:
         alumno = elemento.split('-')
         alumnos.append(alumno)
     print(alumnos[1][0]) 
+
+var = '202202915'
+i = 0
+for alumno in alumnos:
+    if alumno[0] != var:
+        i = i + 1
+    if alumno[0] == var:
+        break
+
+print(alumnos[i])
 
     
